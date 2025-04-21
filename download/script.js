68b85db2e1061c33ae9c98fda582a68f07ea9aff
Shihan <shihanrishad@gmail.com>
2025-04-22 00:35:20 +0600

    Refactor download page: simplify font link, add copy button functionality, and enhance styling for code display

---
+++ download/script.js
@@ -0,0 +1,28 @@
+onload = function() {
+    let copyBtn  = document.querySelector('.copy');
+function copy() {
+    let pre = document.querySelector('pre');
+    let code = pre.textContent;
+
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(code).then(() => {
+            console.log('Code copied to clipboard!');
+        }).catch(err => {
+            console.error('Error copying code: ', err);
+        });
+    } else {
+        let textarea = document.createElement('textarea');
+        textarea.value = code;
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            document.execCommand('copy');
+            console.log('Code copied to clipboard!');
+        } catch (err) {
+            console.error('Error copying code: ', err);
+        }
+        document.body.removeChild(textarea);
+    }
+}
+copyBtn.addEventListener('click', copy);
+}
